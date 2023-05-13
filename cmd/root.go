559fdf6eba6242994fc9978f6111e9de8f88de64
RyotaroSeto <hahahahawaiwai321@gmail.com>
2023-05-13 16:25:02 +0900

    fix root

--- cmd/root.go
+++ cmd/root.go
@@ -27,21 +27,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "neko",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "show cat",
+	Long:  `show cat.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -64,5 +54,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
